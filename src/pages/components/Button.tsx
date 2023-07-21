aedfc79502ba9a3ec10fe1280c6e7f123672a783
techoli <okoliemmanuel47@gmail.com>
2023-07-21 01:56:47 +0100

    Added button2

--- src/pages/components/Button.tsx
+++ src/pages/components/Button.tsx
@@ -4,7 +4,7 @@ import { button } from "../../types/types";
 const Button: React.FC<button> = ({ height, width, text1 }) => {
   return (
     <div
-      className={`font-[poppins] h-[${height}px] w-[${width}px] bg-[#4F46E5] rounded-[8px] text-[white]`}
+      className={`flex justify-center items-center font-[poppins] h-[${height}px] w-[${width}px] bg-[#4F46E5] rounded-[8px] text-[white]`}
     >
       {text1}
     </div>
