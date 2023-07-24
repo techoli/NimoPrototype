a11e0d38242e4c428afc206da604c012ec8e81c6
techoli <okoliemmanuel47@gmail.com>
2023-07-24 10:46:59 +0100

    New Update

--- src/pages/sections/navbar/Navbar.tsx
+++ src/pages/sections/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavData } from "../../../data/data";
 import Button from "../../components/Button";
 import logo from "../../../images/tioologo.png";
+import { AiOutlineDown } from "react-icons/ai";
 
 function Navbar() {
   const handleClick = () => {
@@ -14,9 +15,13 @@ function Navbar() {
         className="absolute left-[22px] top-[5px] w-[130px] h-[100]"
       />
       <div className=" flex w-[650px] m-[auto] h-full  gap-[24px] items-center ">
-        {NavData?.map((item) => (
-          <a href="#" className="font-[600] font-[poppins] text-[18px]">
+        {NavData?.map((item, ind) => (
+          <a
+            href="#"
+            className="font-[600] font-[poppins] text-[18px] flex items-center gap-2"
+          >
             {item.text}
+            {ind === NavData.length - 1 && <AiOutlineDown />}
           </a>
         ))}
       </div>
